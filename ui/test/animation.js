bced3d614bed276c5e1314d951ebd0e305c8ff28
rong_yu <rong_yu@sumilux.cn>
2017-05-05 11:27:36 +0800

    监控系统页

---
+++ ui/test/animation.js
@@ -0,0 +1,9 @@
+/**
+ * Created by foxit on 5/2/17.
+ */
+window.onload = function(){
+    var item2 = document.querySelector(".item2");
+
+    item2.style.top = "550px";
+    item2.style.left = "400px";
+}
