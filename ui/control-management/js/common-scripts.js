0d4ddbc770be2e8a14c538c11d67ce36132bff0f
rong_yu <rong_yu@sumilux.cn>
2018-06-01 16:20:57 +0800

    后台管理系统

---
+++ ui/control-management/js/common-scripts.js
@@ -0,0 +1,29 @@
+/*-------*/
+$(function() {
+  $('#nav-accordion').dcAccordion({
+    eventType: 'click',
+    autoClose: true,
+    saveState: true,
+    disableLink: true,
+    speed: 'slow',
+    showCount: false,
+    autoExpand: true,
+//        cookie: 'dcjq-accordion-1',
+    classExpand: 'dcjq-current-parent'
+  });
+});
+
+// right slidebar
+$(function(){
+  $.slidebars();
+});
+var Script = function () {
+
+    $(function() {
+
+    });
+
+  $('').click(function () {
+
+  });
+}();
